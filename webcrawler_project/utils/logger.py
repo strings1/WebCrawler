e8ab1cc2e6c1bd1fb812fdd2afbd2e4165ec8108
alexandrudarie <alex.darie.cpp@gmail.com>
2025-05-27 23:02:37 +0300

    Added Thread Name to logger, removed pycache

--- webcrawler_project/utils/logger.py
+++ webcrawler_project/utils/logger.py
@@ -11,7 +11,7 @@ def get_logger(name):
     logger = logging.getLogger(name)
     if not logger.handlers:
         handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s: %(message)s')
+        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(name)s [%(threadName)s]: %(message)s')
         handler.setFormatter(formatter)
         logger.addHandler(handler)
         logger.setLevel(logging.DEBUG)
